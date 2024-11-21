e5463902fd91bfe5a272433413f58433212e07fd
l8onrodney <info@leightonrodney.com>
2024-11-21 00:29:40 -0500

    removed neon plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,4 @@
 module.exports = ({ env }) => ({
-    'strapi-neon-tech-db-branches': {
-        enabled: true,
-        config: {
-          neonApiKey: env('NEON_API_KEY'),
-          neonProjectName: env('NEON_PROJECT_NAME'),
-          neonRole: env('NEON_ROLE'),
-          gitBranch: env('GIT_BRANCH'),
-        }
-      },
       upload: {
         config: {
           provider: 'cloudinary',
